42948845453c93cc5ad3f5cd987e5463ffb86693
Anton I. Zolotukhin <anton.i.zolotukhin@gmail.com>
2020-02-23 14:47:02 +0700

    code cleanup

--- grab.py
+++ grab.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 from datetime import datetime
-from  webparser import *
+from webparser import *
 import yaml
 import argparse
 
-start=datetime.now()
+start = datetime.now()
 
 # конфигурируем ArgumentParser
 parser = argparse.ArgumentParser()
@@ -22,20 +25,19 @@ patt.setLinks(structure['data']['links'])
 
 par = XpthParser(patt)
 
-scrp = Scrapper(structure.get('baseurl'),par)
+scrp = Scrapper(structure.get('baseurl'), par)
 
 d = scrp.crawl(structure.get('starturl'))
 
-
 df = pd.DataFrame(columns=patt.DataColumns(), index=[])
-df=df.append(par.data, ignore_index=True)
+df = df.append(par.data, ignore_index=True)
 
-print (df)
+print(df)
 
 with pd.ExcelWriter('reestr.xlsx', engine='xlsxwriter') as writer:
-    df.to_excel(writer,sheet_name='Реестр ПО',index=False)
+    df.to_excel(writer, sheet_name='Реестр ПО', index=False)
     writer.save()
     writer.close()
 #       ---
 
-print ('{} elapsed'.format(datetime.now()-start))
+print('{} elapsed'.format(datetime.now()-start))
--- webparser.py
+++ webparser.py
@@ -1,60 +1,58 @@
-import json
 import re
 from lxml import html
 import requests
 
+
 class XpthPattern:
     def __init__(self):
         self.tree = None
-        self.row=''
+        self.row = ''
         self.data_dict = {}
         self.links = []
 
     def DataColumns(self):
         return self.XPathDataDict().keys()
 
-    def setRowXpath(self,row:str):
+    def setRowXpath(self, row: str):
         self.row = row
 
     def XPathDataDict(self):
         return self.data_dict
 
-    def setXPathDataDict(self, cols:dict):
+    def setXPathDataDict(self, cols: dict):
         self.data_dict = cols
- #       self.df = pd.DataFrame(columns=self.DataColumns(), index=[])
-    def setLinks(self, links:list):
+
+    def setLinks(self, links: list):
         self.links = links
 
 
 class XpthParser:
-    def __init__(self, xppattern:XpthPattern):
+    def __init__(self, xppattern: XpthPattern):
         self.tree = None
 
         self.pattern = xppattern
 
-        self.data=[]
+        self.data = []
 
-    def getTree(self,url:str):
-        print ('fetching ' + url)
+    def getTree(self, url: str):
+        print('fetching ' + url)
         response = requests.get(url, verify=False)
         self.tree = html.fromstring(response.content)
 
-
-    def getXPathChild(self, xpath:str, parent:html.HtmlElement):
+    def getXPathChild(self, xpath: str, parent: html.HtmlElement):
         return parent.xpath(xpath)
 
-    def getXPathElement(self, xpath:str):
+    def getXPathElement(self, xpath: str):
         return self.getXPathChild(xpath, self.tree)
 
-    def getXPathChildContent(self, xpath:str, parent:html.HtmlElement):
-        data=[]
-        href = None
-        for elmnt in self.getXPathChild(xpath,parent):
+    def getXPathChildContent(self, xpath: str, parent: html.HtmlElement):
+        data = []
+        for elmnt in self.getXPathChild(xpath, parent):
             if isinstance(elmnt, str):
                 content = elmnt
             else:
                 content = ''.join(elmnt.itertext())
-            content=re.sub(r'\s+', ' ', content.strip())
+            content = re.sub(r'\s+', ' ', content.strip())
             data.append(content)
         content = '\n'.join(data)
         if content.isdigit():
@@ -65,29 +63,30 @@ class XpthParser:
         return self.tree.xpath(self.pattern.row)
 
     def getAllData(self):
-        parsed_count=0
+        parsed_count = 0
         for row in self.getRows():
             data = {}
-            for key,xpath in self.pattern.XPathDataDict().items():
+            for key, xpath in self.pattern.XPathDataDict().items():
                 data[key] = self.getXPathChildContent(xpath, row)
             if any(data):
                 self.data.append(data)
-                parsed_count+=1
+                parsed_count += 1
         print('{} rows parsed.'.format(parsed_count))
 
+
 class Scrapper:
-    def __init__(self, baseurl:str, parser:XpthParser):
+    def __init__(self, baseurl: str, parser: XpthParser):
         self.baseurl = baseurl
         self.uri = ''
-        self.data = [] # ???
+        self.data = []  # ???
         self.parser = parser
         self.fetchedlinks = []
 
-    def get(self, uri:str):
+    def get(self, uri: str):
         self.parser.getTree(uri)
         self.parser.getAllData()
 
-    def crawl(self,uri:str):
+    def crawl(self, uri: str):
         self.uri = uri
         if self.uri not in self.fetchedlinks:
             self.get(self.baseurl + self.uri)
