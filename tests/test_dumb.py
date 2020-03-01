175d652c3a8e7c943dd7e8afacc21570a45949ff
Anton I. Zolotukhin <anton.i.zolotukhin@gmail.com>
2020-03-01 15:08:27 +0700

    Trying to deploy

---
+++ tests/test_dumb.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+"""
+Don't repeat this, kids. Mmmm-kay?
+"""
+def test_DumbTest():
+
+    assert True
--- xpathwebscrapper/__main__.py
+++ xpathwebscrapper/__main__.py
@@ -1,3 +1,3 @@
-from grab import *
+from xpathwebscrapper.grab import *
 if __name__ == "__main__":
     main()
