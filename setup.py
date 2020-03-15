5ef95e36989de9c84070199bd63305603008ed70
Anton.I.Zolotukhin <anton.i.zolotukhin@gmail.com>
2020-03-15 16:16:04 +0700

    some fix. tests and samples in package now

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@
 from setuptools import setup
 import os
 
-__version__ = '0.2'  # Здесь можно менять глобальный мажор.минор вашего инструмента. Итоговая версия после сборки будет выглядеть так: [major.minor].[build] для релизных сборок и [major.minor.dev]build для нерелизных.
-# В дальнейшем узнать версию вашего установленного инструмента внутри программы возможно используя метод, аналогичный этому:
-# https://github.com/devopshq/FuzzyClassificator/blob/master/FuzzyClassificator.py#L27
-# import pkg_resources  # часть стандартной библиотеки setuptools
-# version = pkg_resources.get_distribution('YourProject').version
+# Здесь можно менять глобальный мажор.минор вашего инструмента.
+__version__ = '0.2'
+# Итоговая версия после сборки будет выглядеть так:
+# [major.minor].[build] для релизных сборок
+# и [major.minor.dev]build для нерелизных.
 
-devStatus = '2 - Pre-Alpha'  # Билд-статус по умолчанию, смотрите: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+# Билд-статус по умолчанию, смотрите: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+devStatus = '2 - Pre-Alpha'
 
 # Логика версионирования в зависимости от веток настраивается ниже:
 if 'TRAVIS_BUILD_NUMBER' in os.environ and 'TRAVIS_BRANCH' in os.environ:
@@ -30,31 +31,35 @@ else:
     print("This is local build")
     __version__ += '.dev0'  # set version as major.minor.localbuild if local build: python setup.py install
 
-print("XpathWebScrapper build version = {}".format(__version__))  # Перед сборкой выведется сообщение о том, какая версия собирается
+# Перед сборкой выведется сообщение о том, какая версия собирается
+print("XpathWebScrapper build version = {}".format(__version__))
 
 #  Это основной раздел настроек setuptools для сборки вашей программы
 setup(
-    name='xpathwebscrapper',  # имя проекта под которым люди будут искать вашу программу в PyPI и инсталлить через "pip install dohq-example-project"
-
+    name='xpathwebscrapper',
     version=__version__,
 
-    description='Simple and easy configurable web scrapper',  # короткое описание проекта - отображается рядом с пакетом в PyPI
-
-    long_description='About XpathWebScrapper: https://github.com/antonzolotukhin/XpathWebScrapper',  # подробная документация должна быть доступна в GitHub Pages по этой ссылке
-
+    # короткое описание проекта - отображается рядом с пакетом в PyPI
+    description='Simple and easy configurable web scrapper',
+    # подробная документация должна быть доступна в GitHub Pages по этой ссылке
+    long_description='About XpathWebScrapper: https://github.com/antonzolotukhin/XpathWebScrapper',
     license='MIT',
 
     author='Anton Zolotukhin',
 
     author_email='anton.i.zolotukhin@gmail.com',
 
-    url='https://github.com/antonzolotukhin/XpathWebScrapper',  # сюда пишем ссылку на GitHub Pages или другой сайт с документацией
-
-    download_url='https://github.com/antonzolotukhin/XpathWebScrapper.git',  # здесь указываем ссылку на проект в GitHub
-
-    entry_points={'console_scripts': ['xpathwebscrapper = xpathwebscrapper.grab:main']},  # Точка входа указывает на основной метод, который нужно запустить при запуске программы из консоли. Например, если основной модуль в пакете exampleproject называется Main, то в данном примере будет запущен метод Main() этого скрипта, если вы наберёте в консоли команду "exampleproject".
-
-    classifiers=[  # все допустимые классификаторы для PyPI подробно перечислены на страничке: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # сюда пишем ссылку на GitHub Pages или другой сайт с документацией
+    url='https://github.com/antonzolotukhin/XpathWebScrapper',
+    # здесь указываем ссылку на проект в GitHub
+    download_url='https://github.com/antonzolotukhin/XpathWebScrapper.git',
+    # Точка входа указывает на основной метод, который нужно запустить при запуске программы из консоли.
+    # Например, если основной модуль в пакете exampleproject называется Main,
+    # то в данном примере будет запущен метод Main() этого скрипта, если вы наберёте в консоли команду "exampleproject"
+    entry_points={'console_scripts': ['xpathwebscrapper = xpathwebscrapper.grab:main']},
+    # все допустимые классификаторы для PyPI подробно перечислены на страничке:
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         'Development Status :: {}'.format(devStatus),
         'Environment :: Console',
         'Intended Audience :: Developers',
@@ -63,14 +68,15 @@ setup(
         'Natural Language :: Russian',
         'Programming Language :: Python :: 3.6',
     ],
-
-    keywords=[  # перечислите все ключевые слова, которые ассоциируются с вашим инструментом, каждое слово отдельной записью
+    # перечислите все ключевые слова, которые ассоциируются с вашим инструментом, каждое слово отдельной записью
+    keywords=[
         'goverenment-data',
         'lxml',
         'website-scrapping',
     ],
-
-    packages=[  # необходимо перечислить ВСЕ каталоги с пакетами, если они присутствуют в вашем проекте, либо оставить '.', что будет указывать на то, что корень проекта сам является пакетом (в корне должен быть __init__.py)
+    # необходимо перечислить ВСЕ каталоги с пакетами, если они присутствуют в вашем проекте, либо оставить '.',
+    # что будет указывать на то, что корень проекта сам является пакетом (в корне должен быть __init__.py)
+    packages=[
         'xpathwebscrapper',
     ],
 
@@ -85,8 +91,9 @@ setup(
         'pytest_httpretty',
         'pytest-mock',
     ],
-
-    install_requires=[  # необходимо перечислить ВСЕ библиотеки, от которых зависит ваш инструмент (requirements), кроме стандартных библиотек, и они будут установлены автоматически при установке вашего инструмента
+    # необходимо перечислить ВСЕ библиотеки, от которых зависит ваш инструмент (requirements),
+    # кроме стандартных библиотек, и они будут установлены автоматически при установке вашего инструмента
+    install_requires=[
             'lxml',
             'pandas',
             'requests',
@@ -96,12 +103,13 @@ setup(
 
     package_data={  # необходимо перечислить ВСЕ файлы, которые должны войти в итоговый пакет, например:
         '': [
-            './xpathwebscrapper/*.py',  # если проект содержит другие модули, их и все входящие в них файлы тоже нужно перечислить
-            './Examples/*.yml', # Examples od site definitions
-            #'./tests/*.py',  # все юнит-тесты, если вы хотите, чтобы люди могли их запускать после установки вашей библиотеки
+            # если проект содержит другие модули, их и все входящие в них файлы тоже нужно перечислить
+            '../Examples/*.yml',  # Examples of site definitions
+            # все юнит-тесты, если вы хотите, чтобы люди могли их запускать после установки вашей библиотеки
+            '../tests/*.py',
 
-            'LICENSE',  # файл лицензии нужно добавить в пакет
-            'README.md',  # файл документации нужно добавить в пакет
+            '../LICENSE',  # файл лицензии нужно добавить в пакет
+            #'../README.md',  # файл документации нужно добавить в пакет
         ],
     },
 
