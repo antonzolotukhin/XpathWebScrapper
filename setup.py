aefda2c616f685e55b9a79fe59a6e66343420da1
Anton.I.Zolotukhin <anton.i.zolotukhin@gmail.com>
2020-03-15 17:41:05 +0700

    flake8 passed

--- setup.py
+++ setup.py
@@ -6,112 +6,95 @@ from setuptools import setup
 import os
 
 # Здесь можно менять глобальный мажор.минор вашего инструмента.
-__version__ = '0.2'
+__version__ = "0.2"
 # Итоговая версия после сборки будет выглядеть так:
 # [major.minor].[build] для релизных сборок
 # и [major.minor.dev]build для нерелизных.
 
 # Билд-статус по умолчанию, смотрите: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-devStatus = '2 - Pre-Alpha'
+devStatus = "2 - Pre-Alpha"
 
 # Логика версионирования в зависимости от веток настраивается ниже:
-if 'TRAVIS_BUILD_NUMBER' in os.environ and 'TRAVIS_BRANCH' in os.environ:
+if "TRAVIS_BUILD_NUMBER" in os.environ and "TRAVIS_BRANCH" in os.environ:
     print("This is TRAVIS-CI build")
-    print("TRAVIS_BUILD_NUMBER = {}".format(os.environ['TRAVIS_BUILD_NUMBER']))
-    print("TRAVIS_BRANCH = {}".format(os.environ['TRAVIS_BRANCH']))
+    print("TRAVIS_BUILD_NUMBER = {}".format(os.environ["TRAVIS_BUILD_NUMBER"]))
+    print("TRAVIS_BRANCH = {}".format(os.environ["TRAVIS_BRANCH"]))
 
-    __version__ += '.{}{}'.format(
-        '' if 'release' in os.environ['TRAVIS_BRANCH'] or os.environ['TRAVIS_BRANCH'] == 'master' else 'dev',
-        os.environ['TRAVIS_BUILD_NUMBER'],
+    __version__ += ".{}{}".format(
+        "" if "release" in os.environ["TRAVIS_BRANCH"] or os.environ["TRAVIS_BRANCH"] == "master" else "dev",
+        os.environ["TRAVIS_BUILD_NUMBER"],
     )
 
-    devStatus = '5 - Production/Stable' if 'release' in os.environ['TRAVIS_BRANCH'] or os.environ['TRAVIS_BRANCH'] == 'master' else devStatus
+    devStatus = (
+        "5 - Production/Stable"
+        if "release" in os.environ["TRAVIS_BRANCH"] or os.environ["TRAVIS_BRANCH"] == "master"
+        else devStatus
+    )
 
 else:
     print("This is local build")
-    __version__ += '.dev0'  # set version as major.minor.localbuild if local build: python setup.py install
+    __version__ += ".dev0"  # set version as major.minor.localbuild if local build: python setup.py install
 
 # Перед сборкой выведется сообщение о том, какая версия собирается
 print("XpathWebScrapper build version = {}".format(__version__))
 
 #  Это основной раздел настроек setuptools для сборки вашей программы
 setup(
-    name='xpathwebscrapper',
+    name="xpathwebscrapper",
     version=__version__,
-
     # короткое описание проекта - отображается рядом с пакетом в PyPI
-    description='Simple and easy configurable web scrapper',
+    description="Simple and easy configurable web scrapper",
     # подробная документация должна быть доступна в GitHub Pages по этой ссылке
-    long_description='About XpathWebScrapper: https://github.com/antonzolotukhin/XpathWebScrapper',
-    license='MIT',
-
-    author='Anton Zolotukhin',
-
-    author_email='anton.i.zolotukhin@gmail.com',
-
+    long_description="About XpathWebScrapper: https://github.com/antonzolotukhin/XpathWebScrapper",
+    license="MIT",
+    author="Anton Zolotukhin",
+    author_email="anton.i.zolotukhin@gmail.com",
     # сюда пишем ссылку на GitHub Pages или другой сайт с документацией
-    url='https://github.com/antonzolotukhin/XpathWebScrapper',
+    url="https://github.com/antonzolotukhin/XpathWebScrapper",
     # здесь указываем ссылку на проект в GitHub
-    download_url='https://github.com/antonzolotukhin/XpathWebScrapper.git',
+    download_url="https://github.com/antonzolotukhin/XpathWebScrapper.git",
     # Точка входа указывает на основной метод, который нужно запустить при запуске программы из консоли.
     # Например, если основной модуль в пакете exampleproject называется Main,
     # то в данном примере будет запущен метод Main() этого скрипта, если вы наберёте в консоли команду "exampleproject"
-    entry_points={'console_scripts': ['xpathwebscrapper = xpathwebscrapper.grab:main']},
+    entry_points={"console_scripts": ["xpathwebscrapper = xpathwebscrapper.grab:main"]},
     # все допустимые классификаторы для PyPI подробно перечислены на страничке:
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: {}'.format(devStatus),
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Topic :: Utilities',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: Russian',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: {}".format(devStatus),
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Russian",
+        "Programming Language :: Python :: 3.6",
     ],
     # перечислите все ключевые слова, которые ассоциируются с вашим инструментом, каждое слово отдельной записью
-    keywords=[
-        'goverenment-data',
-        'lxml',
-        'website-scrapping',
-    ],
+    keywords=["goverenment-data", "lxml", "website-scrapping"],
     # необходимо перечислить ВСЕ каталоги с пакетами, если они присутствуют в вашем проекте, либо оставить '.',
     # что будет указывать на то, что корень проекта сам является пакетом (в корне должен быть __init__.py)
-    packages=[
-        'xpathwebscrapper',
-    ],
-
+    packages=["xpathwebscrapper"],
     setup_requires=[  # необходимо перечислить ВСЕ библиотеки, от которых зависит сборка вашего инструмента
-        'pytest-runner'
+        "pytest-runner"
     ],
-
     tests_require=[  # необходимо перечислить ВСЕ библиотеки, которые должны быть установлены для запуска тестов
-        'requests',
-        'pytest==3.1.2',
-        'HTTPretty',
-        'pytest_httpretty',
-        'pytest-mock',
+        "requests",
+        "pytest==3.1.2",
+        "HTTPretty",
+        "pytest_httpretty",
+        "pytest-mock",
     ],
     # необходимо перечислить ВСЕ библиотеки, от которых зависит ваш инструмент (requirements),
     # кроме стандартных библиотек, и они будут установлены автоматически при установке вашего инструмента
-    install_requires=[
-            'lxml',
-            'pandas',
-            'requests',
-            'pyYAML',
-            'certifi',
-    ],
-
+    install_requires=["lxml", "pandas", "requests", "pyYAML", "certifi"],
     package_data={  # необходимо перечислить ВСЕ файлы, которые должны войти в итоговый пакет, например:
-        '': [
+        "": [
             # если проект содержит другие модули, их и все входящие в них файлы тоже нужно перечислить
-            '../Examples/*.yml',  # Examples of site definitions
+            "../Examples/*.yml",  # Examples of site definitions
             # все юнит-тесты, если вы хотите, чтобы люди могли их запускать после установки вашей библиотеки
-            '../tests/*.py',
-
-            '../LICENSE',  # файл лицензии нужно добавить в пакет
-            #'../README.md',  # файл документации нужно добавить в пакет
+            "../tests/*.py",
+            "../LICENSE",  # файл лицензии нужно добавить в пакет
+            # '../README.md',  # файл документации нужно добавить в пакет
         ],
     },
-
     zip_safe=True,
 )
